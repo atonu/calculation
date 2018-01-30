26bf89c0d2662bdb4dde131719961485f80a1a70
atonu <atonu.zahin@gmail.com>
2018-01-30 11:47:49 +0600

    Initial commit

---
+++ Calculator.lib/basicCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.lib
+{
+    public class basicCalculator
+    {
+        public int add(int arg1, int arg2)
+        {
+            return arg1 + arg2;
+        }
+
+        public int substract(int arg1, int arg2)
+        {
+            return arg1 - arg2;
+        }
+
+        public int multiply(int arg1, int arg2)
+        {
+            return arg1 * arg2;
+        }
+
+        public int divide(int arg1, int arg2)
+        {
+            return arg1 / arg2;
+        }
+    }
+}
---
+++ Calculator.test/run.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.lib;
+
+namespace Calculator.test
+{
+    class run
+    {
+        static int Main(string[] args)
+        {
+            basicCalculator a = new basicCalculator();
+
+
+            Console.WriteLine(a.add(3, 5));
+            Console.WriteLine(a.substract(3, 5));
+            Console.WriteLine(a.multiply(3, 5));
+            Console.WriteLine(a.divide(10, 5));
+
+            return 0;
+        }
+    }
+}
