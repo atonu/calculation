f516aa9937306c04ec01a4e61019d189d12f3347
atonu <atonu.zahin@gmail.com>
2018-01-30 12:17:26 +0600

    test
    test

--- Calculator.test/run.cs
+++ Calculator.test/run.cs
@@ -13,10 +13,13 @@ namespace Calculator.test
         {
             basicCalculator a = new basicCalculator();
 
-
+            Console.WriteLine("Addition");
             Console.WriteLine(a.add(3, 5));
+            Console.WriteLine("Substraction");
             Console.WriteLine(a.substract(3, 5));
+            Console.WriteLine("Multiplication");
             Console.WriteLine(a.multiply(3, 5));
+            Console.WriteLine("Division");
             Console.WriteLine(a.divide(10, 5));
 
             return 0;
